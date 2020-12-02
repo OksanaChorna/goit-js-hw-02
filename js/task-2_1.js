4c80b742b83b5916fcd849ed314792a3ef2fdc00
Oksana Chorna <ksuchorna@gmail.com>
2020-12-02 09:14:14 +0200

    array

--- js/task-2_1.js
+++ js/task-2_1.js
@@ -1,10 +1,13 @@
 const logItems = function(array) {
-  // твой код
+//   твой код
+    console.log(array);
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
+
+
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
