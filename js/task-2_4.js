dc79ef0130bb3ef66d3242fe5746242598661acd
Oksana Chorna <ksuchorna@gmail.com>
2020-12-03 20:12:33 +0200

    Розпочато завдання 2_4

--- js/task-2_4.js
+++ js/task-2_4.js
@@ -1,22 +1,28 @@
-const formatString = function(string) {
-  // твой код
+const formatString = function (string) {
+  const strSplit = string.split("");
+  console.log(strSplit.length);
+  if (strSplit.length <= 40) {
+    return strSplit.join("");
+  } else {
+    return strSplit.splice(40, 1, "...");
+  }
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
 // вернется оригинальная строка
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
 // вернется форматированная строка
 
-console.log(formatString('Curabitur ligula sapien.'));
+console.log(formatString("Curabitur ligula sapien."));
 // вернется оригинальная строка
 
 console.log(
   formatString(
-    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
-  ),
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
 );
 // вернется форматированная строка
