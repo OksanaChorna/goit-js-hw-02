6333c0246862fac1b3b256a51abf189be5a323fa
Oksana Chorna <ksuchorna@gmail.com>
2020-12-04 15:49:46 +0200

    Дз 2_5 зроблено

--- js/task-2_5.js
+++ js/task-2_5.js
@@ -1,14 +1,16 @@
-const checkForSpam = function(message) {
-  // твой код
+const checkForSpam = function (message) {
+  let str = message.toLowerCase();
+
+  return str.includes("spam") || str.includes("sale");
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam("Latest technology news")); // false
 
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
 
-console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam("Get best sale offers now!")); // true
 
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
