da4198d3a8c4f5a0f5b965a75fa8ca7a2abe46cc
Oksana Chorna <ksuchorna@gmail.com>
2020-12-05 13:13:50 +0200

    Дз 2_6 зроблене через do while

--- js/task-2_6.js
+++ js/task-2_6.js
@@ -1,3 +1,22 @@
 let input;
 const numbers = [];
 let total = 0;
+
+numbers.push(input);
+
+do {
+  input = prompt("Введите число");
+  total += Number(input);
+} while (input !== null);
+console.log(`Общая сумма чисел равна ${total}`);
+
+// if (Number.isNaN(input)) {
+//   alert("Было введено не число, попробуйте еще раз");
+// } else {
+//   numbers.push(input);
+// }
+
+// input = prompt("Введите число");
+// for (let i = 0; i <= null; i += 1) {
+//   total += Number(input);
+// }
