d427098863debeed032630ede9397f3a637b4fb7
Oksana Chorna <ksuchorna@gmail.com>
2020-12-05 21:43:37 +0200

    Дз 2_7 зроблене

--- js/task-2_6.js
+++ js/task-2_6.js
@@ -19,14 +19,3 @@ if (numbers.length > 0) {
 }
 
 console.log(`Общая сумма чисел равна ${total}`);
-
-// if (Number.isNaN(input)) {
-//   alert("Было введено не число, попробуйте еще раз");
-// } else {
-//   numbers.push(input);
-// }
-
-// input = prompt("Введите число");
-// for (let i = 0; i <= null; i += 1) {
-//   total += Number(input);
-// }
